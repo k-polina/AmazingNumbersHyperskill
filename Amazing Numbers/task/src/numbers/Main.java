6972ee1efcd340a96369c300d3cfb0a54c79982a
k-polina <kochneva.p@gmail.com>
2022-08-17 14:30:44 +0200

    stage 3

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,7 +1,6 @@
 package numbers;
 
-import java.util.LinkedHashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     private static final Integer BUZZ_NUMBER = 7;
@@ -9,23 +8,53 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a natural number:");
-        int number = scanner.nextInt();
+        Long number = 1L;
 
-        if (number > 0) {
-            LinkedHashMap<String, Boolean> properties = new LinkedHashMap<>();
-            setProperties(number, properties);
-            printResult(properties);
-        } else {
-            System.out.println("This number is not natural!");
+        printGreeting();
+        performCalculations(scanner, number);
+        printExit();
+    }
+
+    private static void performCalculations(Scanner scanner, Long number) {
+        while (number != 0) {
+            number = getInputNumber(scanner);
+
+            if (number > 0) {
+                LinkedHashMap<String, Boolean> properties = new LinkedHashMap<>();
+                setProperties(number, properties);
+                printResult(properties);
+            } else if (number < 0) {
+                System.out.println("The first parameter should be a natural number or zero");
+            }
         }
     }
 
-    private static void setProperties(Integer number, LinkedHashMap<String, Boolean> properties) {
+    private static long getInputNumber(Scanner scanner) {
+        long number;
+        System.out.println("Enter a request: ");
+        number = scanner.nextLong();
+        return number;
+    }
+
+    private static void printExit() {
+        System.out.println("Goodbye!");
+    }
+
+    private static void printGreeting() {
+        System.out.println("""
+                Welcome to Amazing Numbers!
+
+                Supported requests:
+                - enter a natural number to know its properties;
+                - enter 0 to exit.""");
+    }
+
+    private static void setProperties(long number, LinkedHashMap<String, Boolean> properties) {
         properties.put("even", isEven(number));
         properties.put("odd", !isEven(number));
         properties.put("buzz", isBuzz(number));
         properties.put("duck", isDuck(number));
+        properties.put("palindromic", isPalindromic(number));
     }
 
     private static void printResult(LinkedHashMap<String, Boolean> properties) {
@@ -35,15 +64,26 @@ public class Main {
         }
     }
 
-    private static Boolean isDuck(Integer number) {
+    private static Boolean isDuck(Long number) {
         return number.toString().contains(DUCK_NUMBER.toString());
     }
 
-    private static Boolean isBuzz(Integer number) {
+    private static Boolean isBuzz(Long number) {
         return number % BUZZ_NUMBER == 0 || number % 10 == BUZZ_NUMBER;
     }
 
-    private static Boolean isEven(Integer number) {
+    private static Boolean isEven(Long number) {
         return number % 2 == 0;
+    }
+    private static Boolean isPalindromic(Long number) {
+        Long reversed = 0L;
+        Long temp = number;
+        while (temp > 0) {
+            reversed *= 10;
+            reversed += temp % 10;
+            temp /= 10;
+        }
+
+        return reversed.equals(number);
     }
 }
