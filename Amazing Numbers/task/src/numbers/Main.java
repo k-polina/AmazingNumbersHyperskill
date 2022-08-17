0a3e78f810a0c286e0e24930d649f3d72fd69f9d
k-polina <kochneva.p@gmail.com>
2022-08-17 13:36:27 +0200

    stage 2

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,55 +1,49 @@
 package numbers;
 
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Main {
-    private static Integer BUZZ_NUMBER = 7;
+    private static final Integer BUZZ_NUMBER = 7;
+    private static final Integer DUCK_NUMBER = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a natural number:");
-        Integer number = scanner.nextInt();
+        int number = scanner.nextInt();
+
         if (number > 0) {
-            boolean isDivisible = number % BUZZ_NUMBER == 0;
-            boolean doesEnd = number % 10 == BUZZ_NUMBER;
-            String message = getMessage(number, isDivisible, doesEnd);
-            System.out.print(message);
+            LinkedHashMap<String, Boolean> properties = new LinkedHashMap<>();
+            setProperties(number, properties);
+            printResult(properties);
         } else {
             System.out.println("This number is not natural!");
         }
     }
 
-    private static Boolean isBuzz(boolean isDivisible, boolean doesEnd) {
-        return isDivisible || doesEnd;
+    private static void setProperties(Integer number, LinkedHashMap<String, Boolean> properties) {
+        properties.put("even", isEven(number));
+        properties.put("odd", !isEven(number));
+        properties.put("buzz", isBuzz(number));
+        properties.put("duck", isDuck(number));
     }
 
-    private static String getMessage(Integer number, boolean isDivisible, boolean doesEnd) {
-        boolean isBuzz = isBuzz(isDivisible, doesEnd);
-        String isBuzzString = isBuzz ? "" : "not ";
-        String explanation = getExplanation(number, isDivisible, doesEnd, isBuzz);
-        return String.format("This number is %s.\n" +
-                "It is %sa Buzz number.\n" +
-                "Explanation:\n" +
-                        "%s.",
-                getType(number), isBuzzString, explanation);
-    }
-
-    private static String getExplanation(Integer number, boolean isDivisible, boolean doesEnd, boolean isBuzz) {
-        String explanation = number.toString();
-        if (!isBuzz) {
-            explanation += String.format(" is neither divisible by %d nor does it end with %d", BUZZ_NUMBER, BUZZ_NUMBER);
-        } else {
-            if (isDivisible) {
-                explanation += String.format(" is divisible by %d", BUZZ_NUMBER);
-            } if (doesEnd) {
-                explanation += String.format("%s ends with %d", isDivisible ? " and" : "", BUZZ_NUMBER);
-            }
+    private static void printResult(LinkedHashMap<String, Boolean> properties) {
+        System.out.println("Properties of 15");
+        for (String key: properties.keySet()) {
+            System.out.printf("%s: %s\n", key, properties.get(key));
         }
-        return explanation;
     }
 
-    private static String getType(Integer number) {
-        return number % 2 == 0 ? "Even" : "Odd";
+    private static Boolean isDuck(Integer number) {
+        return number.toString().contains(DUCK_NUMBER.toString());
     }
 
+    private static Boolean isBuzz(Integer number) {
+        return number % BUZZ_NUMBER == 0 || number % 10 == BUZZ_NUMBER;
+    }
+
+    private static Boolean isEven(Integer number) {
+        return number % 2 == 0;
+    }
 }
